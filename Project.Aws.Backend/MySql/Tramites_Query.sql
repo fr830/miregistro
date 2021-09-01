c954d9ee63934742de112f6a54ac33aa227f054d
Matias A. Vallejos <matiasavallejos@outlook.com>
2021-09-01 17:40:21 -0300

    Adding project aws backend with SQL Query files

---
+++ Project.Aws.Backend/MySql/Tramites_Query.sql
@@ -0,0 +1,108 @@
+USE miregistro;
+
+/* Cargar categorias tramites*/
+ALTER TABLE categoriatramite AUTO_INCREMENT = 10;
+
+INSERT INTO categoriatramite (Nombre)
+VALUES(('Informe historico de dominio'),
+	  ('Cambio de domicilio con pedido'),
+      ('Canc prenda inc c'),
+	  ('Canc prenda inc a/b'),
+      ('Informe multa e infracciones'),
+	  ('Asignacion de Cedula'),
+      ('Modificacion Prenda'),
+	  ('Informe Estado de Dominio por correo'),
+      ('Transferencia c/pedido'),
+	  ('Fotocopia de constancias registrales'),
+	  ('Informe historico por corresponder'),
+	  ('Reposicion de placa metalica'),
+      ('Devolucion del automotor al titular'),
+	  ('Certificados dominiales'),
+      ('Denuncia de robo'),
+	  ('Dominiales'));
+      
+UPDATE categoriatramite SET Nombre = "Otros" WHERE IdCategoriaTramite = 1;
+
+SELECT * FROM categoriatramite;
+
+/* Cargar etapas tramites */
+INSERT INTO etapatramite (Nombre)
+VALUES ('Observado');
+
+SELECT * FROM etapatramite;
+ALTER TABLE etapatramite AUTO_INCREMENT = 1;
+DELETE FROM etapatramite WHERE IdEtapaTramite = 1;
+
+/* Cargar categoria de errores */
+
+INSERT INTO categoriaerror (Nombre)
+VALUES  ('Error categoria A'),
+		('Error categoria B');
+        
+SELECT * FROM categoriaerror;
+
+INSERT INTO tramite (Dominio, FkIdCategoria, FkIdEmpresa, Fecha, Observaciones)
+VALUES 
+(
+	'AA333XX', /* Dominio */
+	1 /* IdCategoria */,
+    1 /* IdEmpresa */,
+    NOW() /* Fecha */,
+    'Sin observaciones'
+);
+
+SET @last_id_tramite = LAST_INSERT_ID();
+
+ALTER TABLE tramite  AUTO_INCREMENT = 1;
+SELECT * FROM tramite;
+DELETE FROM tramite WHERE IdTramite < 10000;
+DELETE FROM tramite_proceso WHERE IdTramite < 10000;
+
+/* Procesos */
+INSERT INTO tramite_proceso
+VALUES 
+(
+	4 /* TramiteId */,
+    3 /* IdEtapa*/,
+    1 /* IdEmpleado */,
+    True /* Estado actual */
+);
+
+UPDATE tramite_proceso SET FkIdEmpleado = 2 WHERE IdTramite = 1 AND IdEtapaTramite = 1;
+SELECT * FROM tramite_proceso;
+
+/* Errores */
+INSERT INTO tramite_error
+VALUES 
+(
+	1 /* TramiteId */,
+    1 /* IdCategoriaError*/,
+    1 /* IdEmpleado */,
+    False /* Estado actual */
+);
+
+SELECT * FROM tramite_proceso;
+SELECT * FROM empleado;
+
+SELECT T.IdTramite AS ID, T.Dominio AS Dominio, Ct.Nombre AS Categoria, T.Fecha AS Fecha, T.Observaciones AS Observaciones,
+		Et.Nombre AS 'Proceso', P.Nombre AS 'Quien Proceso', 
+		(SELECT Et1.Nombre FROM tramite T1 JOIN tramite_proceso Tp1 ON Tp1.IdTramite = T1.IdTramite
+			JOIN etapatramite Et1 ON Et1.IdEtapaTramite = Tp1.IdEtapaTramite WHERE T1.IdTramite = T.IdTramite AND Et1.IdEtapaTramite = '1') AS Inscripcion,
+        (SELECT P2.Nombre FROM tramite_proceso Tp2 
+		JOIN empleado E2 ON E2.IdEmpleado = Tp2.FkIdEmpleado
+		JOIN usuario U2 ON U2.IdUsuario = E2.FkIdUsuario
+		JOIN perfil P2 ON P2.IdPerfil = U2.FkIdPerfil
+        WHERE Tp2.IdTramite = T.IdTramite AND Tp2.IdEtapaTramite = 2 LIMIT 1) AS 'Quien Inscribio'
+FROM tramite T
+	JOIN categoriatramite Ct ON Ct.IdCategoriaTramite = T.FkIdCategoria
+	JOIN tramite_proceso Tp ON Tp.IdTramite = T.IdTramite
+	JOIN etapatramite Et ON Et.IdEtapaTramite = Tp.IdEtapaTramite
+	JOIN empleado E ON E.IdEmpleado = Tp.FkIdEmpleado
+	JOIN usuario U ON U.IdUsuario = E.FkIdUsuario
+	JOIN perfil P ON P.IdPerfil = U.FkIdPerfil
+GROUP BY T.IdTramite;
+
+SELECT COUNT(*) FROM tramite;
+
+
+
---
+++ Project.Aws.Backend/MySql/Usuarios_Query.sql
@@ -0,0 +1,82 @@
+USE miregistro;
+
+INSERT rol (Nombre, Descripcion) 
+VALUES ('Gestor de estadisticas', 'Permisos para leer estadisticas de empleados');
+	
+SELECT * FROM rol;
+
+UPDATE rol SET Nombre = 'Root', Descripcion = 'Control total del sistema' WHERE IdRol = 3;
+
+ALTER TABLE rol AUTO_INCREMENT = 0;
+ALTER TABLE perfil AUTO_INCREMENT = 0;
+
+DELETE FROM rol WHERE IdRol = 2;
+
+INSERT INTO preguntaseguridad (Pregunta)
+VALUES ('¿Cual era el nombre de tu primer mascota?'),
+('¿Cual es el nombre de la ciudad en la que naciste?'),
+('¿Cual era tu apodo de infancia?'),
+('¿Cual es el nombre de la ciudad en la que se conocieron tus padres?'),
+('¿Cual es el nombre de tu primo mayor?'),
+('¿Como se llamaba la primera escuela a la que asististe?');
+
+SELECT * FROM preguntaseguridad;
+
+/* Insert nuevos usuarios */
+DELETE FROM usuario_Seguridad WHERE IdUsuario < 10;
+DELETE FROM usuario_rol WHERE IdUsuario < 10;
+DELETE FROM empleado WHERE IdEmpleado < 10;
+DELETE FROM perfil WHERE IdPerfil < 10;
+DELETE FROM usuario WHERE IdUsuario < 10;
+
+INSERT INTO usuario(FkIdPerfil, Usuario, Contraseña, Email)
+VALUES(7, 'lauracalafate', 'registrocalafate', 'lauracalafate@hotmail.com');
+
+INSERT INTO perfil (IdPerfil, Nombre, Apellido, Nick, FechaCumpleaños)
+VALUES (7, 'Laura', 'Calafate', 'Lau', NOW());
+
+ALTER TABLE usuario AUTO_INCREMENT = 1;
+
+INSERT INTO empleado (IdEmpleado, FkIdUsuario, FkIdEmpresa, EstadoActual)
+VALUES
+(
+	6,
+	6,
+    1,
+    0
+);
+
+DELETE FROM perfil WHERE idperfil = 7;
+
+UPDATE usuario SET Email = "paolapereyra061@gmail.com" WHERE IdUsuario = 3; 
+
+UPDATE empleado set IdEmpleado = 1 where IdEmpleado = 1001;
+
+SELECT * FROM perfil;
+SELECT * FROM empleado;
+
+/*Autentificar*/
+SELECT * FROM usuario WHERE usuario='noelicalafate' AND contraseña='12345' AND activo = 1 LIMIT 1;
+
+SELECT U.IdUsuario, P.IdPerfil,  U.Usuario, U.Email, U.activo, P.Nombre, P.Apellido, P.nick, P.FechaCumpleaños, Er.nombre
+FROM usuario U
+JOIN perfil P ON P.IdPerfil = U.FkIdPerfil
+JOIN empleado E ON E.FkIdUsuario = U.IdUsuario
+JOIN empresa Er ON Er.IdEmpresa = E.FkIdEmpresa;
+
+ALTER TABLE usuario AUTO_INCREMENT = 1;
+DELETE FROM usuario WHERE idusuario = 2;
+
+/*Rol Insert*/
+INSERT INTO usuario_rol (IdUsuario, IdRol, FechaInicio, FechaFin)
+VALUES (3, 4, NOW(), '2022-12-01');
+
+/*Rol Query*/
+SELECT U.Usuario, R.IdRol, R.Nombre
+FROM usuario_rol Ur
+INNER JOIN usuario U ON U.IdUsuario = Ur.IdUsuario
+INNER JOIN rol R ON R.IdRol = Ur.IdRol
+WHERE Ur.FechaFin >= NOW();
+
+UPDATE rol SET Nombre = "Gestor caja" WHERE IdRol = "6";
+
